e02c9d92c687a33ca762793d85e47d5df95fede4
Takuya Akiba <t.akiba.65536@gmail.com>
2022-09-02 23:01:17 +0900

    とりあえず一旦単なる平均を出すbest_color

--- src/bin/iwiwi.rs
+++ src/bin/iwiwi.rs
@@ -1,10 +1,14 @@
 use icfpc2022;
 
-
-
 fn main() {
     let input = std::env::args().nth(1).unwrap();
     let png = icfpc2022::read_png(&input);
 
-    dbg!(icfpc2022::color::best_color(&png, 0, 0, 10, 10));
+    dbg!(icfpc2022::color::best_color(
+        &png,
+        0,
+        png[0].len(),
+        0,
+        png.len()
+    ));
 }
--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,33 @@
-pub fn best_color(png: &Vec<Vec<[u8; 4]>>, lx: usize, rx: usize, ly: usize, ry: usize) -> ([u8; 4], f64) {
-    unimplemented!();
+pub fn best_color(
+    png: &Vec<Vec<[u8; 4]>>,
+    lx: usize,
+    rx: usize,
+    ly: usize,
+    ry: usize,
+) -> ([u8; 4], f64) {
+    let mut s: [f64; 4] = [0.0; 4];
+
+    for y in ly..ry {
+        for x in lx..rx {
+            for c in 0..4 {
+                s[c] += png[y][x][c] as f64;
+            }
+        }
+    }
+
+    let color = s.map(|x| (x / ((rx - lx) as f64) / ((ry - ly) as f64)) as u8);
+
+    let mut cost = 0.0;
+    for y in ly..ry {
+        for x in lx..rx {
+            let mut t = 0.0;
+            for c in 0..4 {
+                let d = (color[c] as f64) - (png[y][x][c] as f64);
+                t += d * d
+            }
+            cost += t.sqrt();
+        }
+    }
+
+    return (color, cost);
 }
