5b26404c0c15d8d69cf0ec2d729a93b099dc5bb6
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-03 08:37:51 +0900

    median monalisa

---
+++ src/bin/median_monalisa.rs
@@ -0,0 +1,16 @@
+use icfpc2022::{color::best_color2, read_png, write_png, Color};
+
+fn main() {
+    let png = read_png("problems/16.png");
+    let (color, cost) = best_color2(&png, 0, 400, 0, 400);
+    dbg!((color, cost));
+
+    let png = read_png("problems/9.png");
+    let mut small = vec![vec![Color::default(); 40]; 40];
+    for (y, row) in small.iter_mut().enumerate() {
+        for (x, px) in row.iter_mut().enumerate() {
+            *px = best_color2(&png, 10 * x, 10 * (x + 1), 10 * y, 10 * (y + 1)).0;
+        }
+    }
+    write_png("tmp/9-small.png", small).unwrap();
+}
