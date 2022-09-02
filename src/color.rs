169bf94070311a0da6b4a95549c4d22db235e19c
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-03 07:30:10 +0900

    fix

--- src/color.rs
+++ src/color.rs
@@ -116,27 +116,35 @@ fn geometric_median_4d(points: &[[f64; 4]]) -> [f64; 4] {
     for i in 0..4 {
         x[i] /= n as f64;
     }
-    for eps in [1e2, 1.0, 0.1, 1e-3] {
-        // TODO
-        let dists = points
-            .iter()
-            .map(|p| (0..4).map(|i| (p[i] - x[i]).powi(2)).sum::<f64>().sqrt())
-            .collect::<Vec<_>>();
-        let dist = dists.iter().sum::<f64>();
+    // TODO: fix eps
+    for iter in 0..20 {
+        let eps = if iter < 10 { 1.0 } else { 0.1 };
+        // let dists = points
+        //     .iter()
+        //     .map(|p| (0..4).map(|i| (p[i] - x[i]).powi(2)).sum::<f64>().sqrt())
+        //     .collect::<Vec<_>>();
+        // let dist = dists.iter().sum::<f64>();
         // dbg!(&dists, dist);
 
-        let x0 = x;
-        x = [0.0; 4];
+        // dbg!(&x);
+
+        // let x0 = x;
+        // x = [0.0; 4];
+        let mut grad = [0.0; 4];
         let mut w_sum = 0.0;
         for p in points {
-            let w = 1.0 / (eps + (0..4).map(|i| (p[i] - x0[i]).powi(2)).sum::<f64>().sqrt());
+            let diff = [p[0] - x[0], p[1] - x[1], p[2] - x[2], p[3] - x[3]];
+            let dist = diff.iter().map(|d| d.powi(2)).sum::<f64>().sqrt();
+            let w = 1.0 / (eps + dist);
             w_sum += w;
             for i in 0..4 {
-                x[i] += w * p[i];
+                grad[i] += w * diff[i];
             }
         }
+        dbg!(w_sum);
         for i in 0..4 {
-            x[i] /= w_sum;
+            // x[i] /= w_sum;
+            x[i] += grad[i] / w_sum;
         }
     }
     x
@@ -149,13 +157,16 @@ mod tests {
 
     #[test]
     fn test_geometric_median_4d() {
-        let points = vec![
-            [0.0, 1.0, 2.0, 3.0],
-            [4.0, 3.0, 2.0, 1.0],
-            [3.0, 2.0, 7.0, 7.0],
-        ];
+        let a = [0.0, 10.0, 20.0, 30.0];
+        let b = [40.0, 30.0, 20.0, 10.0];
+        let c = [30.0, 20.0, 70.0, 70.0];
+        // let points = vec![a, b, c];
+        // let median = geometric_median_4d(&points);
+        // dbg!(median);
+
+        let points = vec![c, c, a, b, c, a, c];
         let median = geometric_median_4d(&points);
-        dbg!(median);
-        // assert!(false);
+        // dbg!(median);
+        assert_eq!(median.map(|v| v.round()), c);
     }
 }
