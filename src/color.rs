ec887a7bd0ae0e0126fd3313bdb6fe4f2da15f1c
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-03 00:30:53 +0900

    wip: geom median

--- src/color.rs
+++ src/color.rs
@@ -70,3 +70,90 @@ pub fn mode_color(
 
     return (color, cost);
 }
+
+pub fn best_color2(
+    png: &Vec<Vec<[u8; 4]>>,
+    lx: usize,
+    rx: usize,
+    ly: usize,
+    ry: usize,
+) -> ([u8; 4], f64) {
+    let mut points = Vec::with_capacity((ry-ly) * (rx-lx));
+    for y in ly..ry {
+        for x in lx..rx {
+            let p = &png[y][x];
+            points.push([p[0] as f64, p[1] as f64, p[2] as f64, p[3] as f64]);
+        }
+    }
+
+    let color = geometric_median_4d(&points);
+    let color = color.map(|v| v as u8);
+
+    // copy-paste...
+    let mut cost = 0.0;
+    for y in ly..ry {
+        for x in lx..rx {
+            let mut t = 0.0;
+            for c in 0..4 {
+                let d = (color[c] as f64) - (png[y][x][c] as f64);
+                t += d * d
+            }
+            cost += t.sqrt();
+        }
+    }
+
+    return (color, cost);
+}
+
+
+fn geometric_median_4d(points: &[[f64; 4]]) -> [f64; 4] {
+    let n = points.len();
+    let mut x = [0.0; 4];
+    for p in points {
+        for i in 0..4 {
+            x[i] += p[i];
+        }
+    }
+    for i in 0..4 {
+        x[i] /= n as f64;
+    }
+    for eps in [1e2, 1.0, 0.1, 1e-3] {  // TODO
+        let dists = points.iter().map(|p| (0..4).map(|i| (p[i] - x[i]).powi(2)).sum::<f64>().sqrt()).collect::<Vec<_>>();
+        let dist = dists.iter().sum::<f64>();
+        // dbg!(&dists, dist);
+
+        let x0 = x;
+        x = [0.0; 4];
+        let mut w_sum = 0.0;
+        for p in points {
+            let w = 1.0 / (eps + (0..4).map(|i| (p[i] - x0[i]).powi(2)).sum::<f64>().sqrt());
+            w_sum += w;
+            for i in 0..4 {
+                x[i] += w * p[i];
+            }
+        }
+        for i in 0..4 {
+            x[i] /= w_sum;
+        }
+    }
+    x
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_geometric_median_4d() {
+        let points = vec![
+            [0.0, 1.0, 2.0, 3.0],
+            [4.0, 3.0, 2.0, 1.0],
+            [3.0, 2.0, 7.0, 7.0],
+        ];
+        let median = geometric_median_4d(&points);
+        dbg!(median);
+        // assert!(false);
+    }
+}
