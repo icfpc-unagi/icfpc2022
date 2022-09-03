67eff35aec7481fa95ffafea307c7595a6c67a61
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-03 20:55:36 +0900

    refactor

--- src/bin/trim_end.rs
+++ src/bin/trim_end.rs
@@ -2,9 +2,8 @@ use std::fs::File;
 
 use icfpc2022::{read_isl, read_png, write_isl, Canvas};
 
-fn main1(problem_id: u32, id: &str) -> anyhow::Result<()> {
-    let mut f = File::open(format!("submissions/{id}.isl"))?;
-    // let id = format!("{problem_id}-{id}");
+fn main1(problem_id: u32, submission_id: u32) -> anyhow::Result<()> {
+    let mut f = File::open(format!("submissions/{submission_id}.isl"))?;
 
     let mut program = read_isl(&mut f)?;
     let mut changed = false;
@@ -17,7 +16,6 @@ fn main1(problem_id: u32, id: &str) -> anyhow::Result<()> {
         return Ok(());
     }
 
-    dbg!(format!("probrems/{problem_id}.png"));
     let answer = read_png(&format!("problems/{problem_id}.png"));
     let score = Canvas::new400().apply_all_and_score(program.clone(), &answer)?;
     let mut f_out = File::create(format!("out/opt_{}_{:06.0}", problem_id, score))?;
@@ -26,31 +24,28 @@ fn main1(problem_id: u32, id: &str) -> anyhow::Result<()> {
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut best_submissions = std::collections::BTreeMap::<u32, (u32, String)>::new();
+    let mut best_submissions = std::collections::BTreeMap::<u32, icfpc2022::Submission>::new();
 
     // from local_optimize_all.rs
     for json_path in glob::glob("submissions/*.json")? {
         let json_path = json_path?;
-        let submission_id = json_path.file_name().unwrap().to_str().unwrap();
-        let submission_id = submission_id[..submission_id.find('.').unwrap()].to_string();
-
         let submission: icfpc2022::Submission =
             serde_json::from_reader(std::fs::File::open(json_path)?)?;
-
-        let val = (submission.cost, submission_id);
 
         best_submissions
             .entry(submission.problem_id)
             .and_modify(|s| {
-                if s.0 > val.0 {
-                    *s = val.clone()
+                if s.cost > submission.cost {
+                    *s = submission.clone()
                 }
             })
-            .or_insert(val);
+            .or_insert(submission);
     }
-    for (&problem_id, (_, submission_id)) in best_submissions.iter() {
+
+    for (&problem_id, submission) in best_submissions.iter() {
+        let submission_id = submission.id;
         println!("problem={problem_id}, submission={submission_id}");
-        main1(problem_id, &submission_id)?;
+        main1(problem_id, submission_id)?;
     }
     Ok(())
 }
