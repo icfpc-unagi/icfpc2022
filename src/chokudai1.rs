eb1fdec9a616187ae0d46417e671989898ce1247
Naohiro Takahashi <chokudai@gmail.com>
2022-09-03 15:58:52 +0900

    添え時修正

--- src/chokudai1.rs
+++ src/chokudai1.rs
@@ -222,7 +222,7 @@ pub fn solve_swap(png: &mut Vec<Vec<[u8; 4]>>, border: f64, combo: usize) -> (f6
                     v.push(block);
                 }
 
-                moves2.push(Move::Swap(v[p1].clone(), v[p2].clone()));
+                moves2.push(Move::Swap(v[le - 1 - p2].clone(), v[le - 1 - p1].clone()));
 
                 for i in 0..le {
                     if i == le - 1 - p1 {
@@ -281,7 +281,7 @@ pub fn solve_swap(png: &mut Vec<Vec<[u8; 4]>>, border: f64, combo: usize) -> (f6
                 v.push(block);
             }
 
-            moves2.push(Move::Swap(v[p1].clone(), v[p2].clone()));
+            moves2.push(Move::Swap(v[le - 1 - p1].clone(), v[le - 1 - p2].clone()));
 
             for i in 0..le {
                 if i == le - 1 - p1 {
