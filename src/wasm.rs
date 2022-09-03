af6792bf6b4f61baa01d8baf6afcfdc9e82a8cb5
sulume <sulume@gmail.com>
2022-09-03 07:39:22 +0000

    wasm init for canvas with isl

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,7 +1,7 @@
 // NOTE: To enable completion, add vscode settings "rust-analyzer.cargo.target": "wasm32-unknown-unknown"
 use crate::*;
-use wasm_bindgen::{prelude::*, Clamped};
-use web_sys::{CanvasRenderingContext2d, ImageData};
+use wasm_bindgen::{prelude::*, *};
+use web_sys::*;
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -9,7 +9,45 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvases = document.query_selector_all("canvas[isl]")?;
+    for i in 0..canvases.length() {
+        let canvas = JsValue::from(canvases.get(i));
+        let isl = Element::from(canvas.clone()).get_attribute("isl").unwrap();
+        let mut managed = ManagedCanvas::new(canvas.into());
+        managed.apply(&isl)?;
+        managed.render()?;
+    }
     Ok(())
+}
+
+#[wasm_bindgen]
+pub struct ManagedCanvas {
+    ctx: CanvasRenderingContext2d,
+    model: Canvas,
+}
+
+#[wasm_bindgen]
+impl ManagedCanvas {
+    #[wasm_bindgen(constructor)]
+    pub fn new(el_canvas: HtmlCanvasElement) -> Self {
+        Self {
+            ctx: JsValue::from(el_canvas.get_context("2d").unwrap()).into(),
+            model: Canvas::new400(),
+        }
+    }
+
+    pub fn render(&self) -> Result<(), JsValue> {
+        self.ctx.clear_rect(0.0, 0.0, 0.0, 0.0);
+        render_bitmap(&self.ctx, &self.model.bitmap)
+    }
+
+    pub fn apply(&mut self, isl: &str) -> Result<(), JsValue> {
+        let isl = read_isl(isl.as_bytes()).unwrap();
+        self.model.apply_all(isl);
+        self.render()
+    }
 }
 
 #[wasm_bindgen]
