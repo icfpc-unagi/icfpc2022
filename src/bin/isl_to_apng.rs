e2068fe31125623dd05ac792f84f416ad8436e09
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-03 15:54:57 +0900

    png all best

--- src/bin/isl_to_apng.rs
+++ src/bin/isl_to_apng.rs
@@ -2,8 +2,8 @@ use std::{fs::File, io};
 
 use icfpc2022::{flatten_png_data, read_isl, write_png, Canvas, Move};
 
-fn main() -> anyhow::Result<()> {
-    let mut f = File::open("submissions/7600.isl")?;
+fn main1(id: &str) -> anyhow::Result<()> {
+    let mut f = File::open(format!("submissions/{id}.isl"))?;
     // let mut buf = vec![];
     // f.read_to_end(&mut buf)?;
     // let s = String::from_utf8(buf)?;
@@ -16,7 +16,7 @@ fn main() -> anyhow::Result<()> {
         .count(); // program.len() + 1;
 
     let mut encoder = png::Encoder::new(
-        io::BufWriter::new(File::create("tmp/b-animated.png")?),
+        io::BufWriter::new(File::create(format!("tmp/{id}-animated.png"))?),
         400,
         400,
     );
@@ -42,6 +42,36 @@ fn main() -> anyhow::Result<()> {
         }
     }
     // eprintln!("{:?}", program);
-    write_png("tmp/b.png", canvas.bitmap)?;
+    write_png(&format!("tmp/{id}.png"), canvas.bitmap)?;
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut best_submissions = std::collections::BTreeMap::<u32, (u32, String)>::new();
+
+    // from local_optimize_all.rs
+    for json_path in glob::glob("submissions/*.json")? {
+        let json_path = json_path?;
+        let submission_id = json_path.file_name().unwrap().to_str().unwrap();
+        let submission_id = submission_id[..submission_id.find('.').unwrap()].to_string();
+
+        let submission: icfpc2022::Submission =
+            serde_json::from_reader(std::fs::File::open(json_path)?)?;
+
+        let val = (submission.cost, submission_id);
+
+        best_submissions
+            .entry(submission.problem_id)
+            .and_modify(|s| {
+                if s.0 > val.0 {
+                    *s = val.clone()
+                }
+            })
+            .or_insert(val);
+    }
+    for (&problem_id, (_, submission_id)) in best_submissions.iter() {
+        println!("problem={problem_id}, submission={submission_id}");
+        main1(&submission_id);
+    }
     Ok(())
 }
