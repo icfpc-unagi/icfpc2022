cb312444a42b65d38613502efdce140c26d10145
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2022-09-04 15:45:26 +0900

    .

--- go/internal/handler/scoreboard.go
+++ go/internal/handler/scoreboard.go
@@ -3,10 +3,11 @@ package handler
 import (
 	"bytes"
 	"fmt"
-	"github.com/icfpc-unagi/icfpc2022/go/internal/official"
 	"html"
 	"net/http"
 	"sort"
+
+	"github.com/icfpc-unagi/icfpc2022/go/internal/official"
 )
 
 func init() {
@@ -17,7 +18,7 @@ func scoreboardTemplate(w http.ResponseWriter, r *http.Request) {
 	buf := &bytes.Buffer{}
 	defer func() { Template(w, buf.Bytes()) }()
 
-	fmt.Fprintf(buf, "<h1>Scoreboard</h1>")
+	fmt.Fprintf(buf, "<h1>順位表</h1>")
 
 	scoreboard, err := official.Scoreboard()
 	if err != nil {
@@ -25,6 +26,66 @@ func scoreboardTemplate(w http.ResponseWriter, r *http.Request) {
 			html.EscapeString(fmt.Sprintf("%+v", err)))
 		return
 	}
+
+	thresholds := []int{35, 30, 25, 20}
+	type rankingRecord struct {
+		TeamName string
+		Score    int
+	}
+	rankings := make([][]*rankingRecord, len(thresholds))
+
+	for _, u := range scoreboard.Users {
+		solved := map[int]int{}
+		for _, r := range u.Results {
+			if r.SubmissionCount != 0 {
+				solved[r.ProblemID] = r.MinCost
+			}
+		}
+
+		for tIndex, t := range thresholds {
+			score := 0
+			for i := 1; i <= t; i++ {
+				if s, ok := solved[i]; ok {
+					score = score + s
+					continue
+				}
+				score = -1
+				break
+			}
+			if score > 0 {
+				rankings[tIndex] = append(rankings[tIndex], &rankingRecord{
+					TeamName: u.TeamName,
+					Score:    score,
+				})
+			}
+		}
+	}
+	for _, r := range rankings {
+		sort.SliceStable(r, func(i, j int) bool {
+			return r[i].Score < r[j].Score
+		})
+	}
+
+	fmt.Fprintf(buf, "<h2>レベル別ランキング</h2>")
+	fmt.Fprintf(buf, `<table style="width: 100%%; table-layout: fixed; white-space: nowrap; font-size: 70%%"><tr><td style="width: 5ex">順位</td>`)
+	for _, t := range thresholds {
+		fmt.Fprintf(buf, `<td width="30%%">%d問級</td><td style="width: 8ex"></td>`, t)
+	}
+	fmt.Fprintf(buf, `</tr>`)
+
+	for i := 0; i < 20; i++ {
+		fmt.Fprintf(buf, `<tr><td>%d 位</td>`, i+1)
+		for t := range thresholds {
+			style := "overflow: hidden;"
+			if rankings[t][i].TeamName == "Unagi" {
+				style += "background: #cdf; color: red; font-weight: bold;"
+			}
+			fmt.Fprintf(buf, `<td style="%s">%s</td>`, style, html.EscapeString(rankings[t][i].TeamName))
+			fmt.Fprintf(buf, `<td style="%s; text-align: right">%d</td>`, style, rankings[t][i].Score)
+		}
+		fmt.Fprintf(buf, "</tr>")
+	}
+	fmt.Fprintf(buf, "</table>")
 
 	problems := []int{}
 	problemCosts := map[int][]int{}
@@ -52,6 +113,7 @@ func scoreboardTemplate(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	fmt.Fprintf(buf, "<h2>スコア詳細</h2>")
 	fmt.Fprint(buf,
 		`凡例: <span style="color:red;font-weight:bold">1位</span>`+
 			` <span style="color:#880;font-weight:bold">5位以内</span>`+
