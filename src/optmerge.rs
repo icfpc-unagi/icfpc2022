bec81ea6c56a629c724a61c9e42d56fd16ec83ed
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-04 17:46:42 +0900

    wip3: optmerge

--- src/optmerge.rs
+++ src/optmerge.rs
@@ -38,16 +38,18 @@ fn merge_all_safe(canvas: &mut Canvas) -> anyhow::Result<Vec<Move>> {
     //     bail!("not implemented yet...");
     // }
 
-    merge_all_internal(h, w);
+    let (cost, path) = merge_all_internal(h, w);
+    eprintln!("{:?}", path);
 
     let mut moves = vec![];
-    Ok((moves))
+    Ok(moves)
 }
 
-fn merge_all_internal(h: usize, w: usize) {
+fn merge_all_internal(h: usize, w: usize) -> (i32, Vec<(usize, usize, i8)>) {
     const INF: i32 = 1_000_000_000;
-    let mut cost0 = mat![INF; h+1; w+1];
-    let mut cost1 = mat![INF; h+1; w+1];
+    // (min_cost, reproduce_key)
+    let mut cost0 = mat![(INF, -1); h+1; w+1];
+    let mut cost1 = mat![(INF, -1); h+1; w+1];
 
     let cost_scale = (h * w) as f64;
     let cut_cost = |a: usize, b: usize| (7.0 * cost_scale / ((a * b) as f64)).round() as i32;
@@ -58,44 +60,77 @@ fn merge_all_internal(h: usize, w: usize) {
         .rev()
     {
         dbg!(a, b);
-        if (a, b) == (h, w) {
-            cost0[a][b] = 0;
-            cost1[a][b] = 0;
+        if a == h {
+            cost0[a][b].0 = 0;
+        }
+        if b == w {
+            cost1[a][b].0 = 0;
+        }
+        if a == h || b == w {
             continue;
         }
         if a < h {
+            let rep = 0;
             let common_cost = {
                 let mut c: i32 = (b.max(1)..w).map(|bb| merge_cost(1, bb)).sum();
                 if a > 0 {
                     c += merge_cost(a, w);
                 }
-                c += cost0[a + 1][b];
+                c += cost0[a + 1][b].0;
                 c
             };
             if b == 0 {
-                cost0[a][b].setmin(common_cost);
+                cost0[a][b].setmin((common_cost, rep));
             } else {
-                cost0[a][b].setmin(common_cost + cut_cost(h - a, b));
-                cost1[a][b].setmin(common_cost + cut_cost(h, b) + cut_cost((a + 1).max(h - a), b));
+                cost0[a][b].setmin((common_cost + cut_cost(h - a, b), rep));
+                cost1[a][b].setmin((
+                    common_cost + cut_cost(h, b) + cut_cost((a + 1).max(h - a), b),
+                    rep,
+                ));
             }
         }
         if b < w {
+            let rep = 1;
             let common_cost = {
                 let mut c: i32 = (a.max(1)..h).map(|aa| merge_cost(aa, 1)).sum();
                 if b > 0 {
                     c += merge_cost(h, b);
                 }
-                c += cost1[a][b + 1];
+                c += cost1[a][b + 1].0;
                 c
             };
             if a == 0 {
-                cost1[a][b].setmin(common_cost);
+                cost1[a][b].setmin((common_cost, rep));
             } else {
-                cost1[a][b].setmin(common_cost + cut_cost(a, w - b));
-                cost0[a][b].setmin(common_cost + cut_cost(a, w) + cut_cost(a, (b + 1).max(w - b)));
+                cost1[a][b].setmin((common_cost + cut_cost(a, w - b), rep));
+                cost0[a][b].setmin((
+                    common_cost + cut_cost(a, w) + cut_cost(a, (b + 1).max(w - b)),
+                    rep,
+                ));
             }
         }
         dbg!(cost0[a][b]);
         dbg!(cost1[a][b]);
+    }
+    let mut a = 0;
+    let mut b = 0;
+    let (total_cost, mut r) = std::cmp::min(cost0[a][b], cost1[a][b]);
+    let mut ret = vec![];
+    loop {
+        ret.push((a, b, r));
+        match r {
+            0 => {
+                a += 1;
+                r = cost0[a][b].1;
+            }
+            1 => {
+                b += 1;
+                r = cost1[a][b].1;
+            }
+            _ => {
+                assert!(a == h || b == w);
+                return (total_cost, ret);
+            }
+        }
     }
 }
