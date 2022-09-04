72d150171f4e4ef68d583d78b42f83df7211eea8
Takuya Akiba <t.akiba.65536@gmail.com>
2022-09-04 09:56:02 +0900

    fn find_best_submissionsをつくった、rotateのinitialへの対応を先にやります

--- src/bin/local_optimize_all.rs
+++ src/bin/local_optimize_all.rs
@@ -1,30 +1,11 @@
 fn main() -> anyhow::Result<()> {
-    let mut best_submissions = std::collections::BTreeMap::<u32, icfpc2022::Submission>::new();
-
-    for json_path in glob::glob("submissions/*.json")? {
-        let submission: icfpc2022::Submission =
-            serde_json::from_reader(std::fs::File::open(json_path?)?)?;
-
-        best_submissions
-            .entry(submission.problem_id)
-            .and_modify(|s| {
-                if s.cost > submission.cost {
-                    *s = submission.clone()
-                }
-            })
-            .or_insert(submission);
-    }
+    let best_submissions = icfpc2022::submissions::find_best_submissions()?;
 
     let mut gain = 0.0;
 
     for (problem_id, submission) in best_submissions.iter() {
-        // if *problem_id == 1 {
-        //     continue;
-        // }
-
         let (submission, program, image) =
             icfpc2022::local_optimization::read_submission(submission.id)?;
-
         let (program, score) = icfpc2022::local_optimization::optimize(program, &image, 10, true);
 
         //let program = icfpc2022::local_optimization::optimize_color(program.clone(), &image);
--- src/submissions.rs
+++ src/submissions.rs
@@ -1,1 +1,20 @@
+pub fn find_best_submissions() -> anyhow::Result<std::collections::BTreeMap<u32, super::Submission>>
+{
+    let mut best_submissions = std::collections::BTreeMap::<u32, super::Submission>::new();
 
+    for json_path in glob::glob("submissions/*.json")? {
+        let submission: super::Submission =
+            serde_json::from_reader(std::fs::File::open(json_path?)?)?;
+
+        best_submissions
+            .entry(submission.problem_id)
+            .and_modify(|s| {
+                if s.cost > submission.cost {
+                    *s = submission.clone()
+                }
+            })
+            .or_insert(submission);
+    }
+
+    anyhow::Ok(best_submissions)
+}
