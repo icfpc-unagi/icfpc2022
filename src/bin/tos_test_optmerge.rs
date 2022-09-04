25d5cc41cf3a86bcf65eb3081b611ecae008b420
Toshiki Kataoka <tos.lunar@gmail.com>
2022-09-04 20:39:36 +0900

    run all

--- src/bin/tos_test_optmerge.rs
+++ src/bin/tos_test_optmerge.rs
@@ -3,8 +3,13 @@ use std::fs::File;
 use icfpc2022::{load_problem, optmerge::merge_all, write_isl};
 
 fn main() -> anyhow::Result<()> {
-    let (mut canvas, _) = load_problem(26);
-    let moves = merge_all(&mut canvas);
-    write_isl(File::create("out/tos_merge26.isl")?, moves)?;
+    for problem_id in 26..=35 {
+        let (mut canvas, _) = load_problem(problem_id);
+        let moves = merge_all(&mut canvas);
+        write_isl(
+            File::create(format!("out/tos_merge{problem_id}.isl"))?,
+            moves,
+        )?;
+    }
     Ok(())
 }
