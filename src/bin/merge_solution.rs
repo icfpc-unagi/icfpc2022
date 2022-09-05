85792a14f36a531cc810dac39d27ce4bf63e74b2
Yoichi Iwata <wataorzorz@gmail.com>
2022-09-05 12:32:32 +0900

    optimize済みをマージすると盤面が白想定なのでゴミが残るから白塗りした

--- src/bin/merge_solution.rs
+++ src/bin/merge_solution.rs
@@ -1,4 +1,7 @@
-use icfpc2022::{submissions::find_best_submission, *};
+use icfpc2022::{
+    submissions::{find_best_submission, read_submission_program},
+    *,
+};
 
 fn main() {
     let from = [5, 2, 10, 18, 11, 24, 9, 15, 7, 25];
@@ -6,10 +9,16 @@ fn main() {
         let problem_id = (26 + i) as u32;
         let (mut init_canvas, png) = load_problem(problem_id);
         let best = find_best_submission(from[i]).unwrap().id;
-        let s1 = read_isl(std::fs::File::open(format!("out/tos_merge{}.isl", problem_id)).unwrap())
-            .unwrap();
-        let s2 =
-            read_isl(std::fs::File::open(format!("submissions/{}.isl", best)).unwrap()).unwrap();
+        let mut s1 =
+            read_isl(std::fs::File::open(format!("out/tos_merge{}.isl", problem_id)).unwrap())
+                .unwrap();
+        let mut tmp = init_canvas.clone();
+        tmp.apply_all(s1.clone());
+        s1.push(Move::Color(
+            BlockId(vec![tmp.counter]),
+            [255, 255, 255, 255],
+        ));
+        let s2 = read_submission_program(best).unwrap().1;
         let merged = wata::merge_solution(&init_canvas, &s1, &s2);
         eprintln!(
             "{}: {}",
